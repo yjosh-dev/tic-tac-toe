985ce39f9805b7a1dd0b95cf5e6845cbef8edc7d
Kirei <132071917+yjosh-dev@users.noreply.github.com>
2026-06-08 14:41:40 +0800

    Remove experimental settings for Vercel

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['10.47.126.79', 'localhost:3000'],
-  },
 };
 
 export default nextConfig;
